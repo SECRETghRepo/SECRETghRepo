6690e6c19e486154ee8922bee8e4f92d2db3fad9
Vivek Kumar <vivek.kumar.cse20@itbhu.ac.in>
2021-06-26 09:18:10 +0530

    Created files for components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
-import {Menu} from './components/Menu'
 import './App.css';
+import {BrowserRouter, Route, Switch} from 'react-router-dom';
+import {Navbar} from './components/Navbar';
+import {Tree} from './components/Tree'
+import {Home} from './components/Home'
 
 function App() {
   return (
     <div className='App'>
+      <BrowserRouter>
+          <Switch>
+            <Navbar/>
+            <Route exact path = '/tree'>
+              <Tree />
+            </Route>
+            <Route exact path = '/home'>
+              <Home />
+            </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ src/components/Tree.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export const Tree = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
