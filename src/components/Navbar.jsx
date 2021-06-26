7c2c5eb39b0f6ba5faddda8241cfe63286c3a58f
Vivek Kumar <vivek.kumar.cse20@itbhu.ac.in>
2021-06-26 20:02:39 +0530

    Added Navebar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
+import {NavLink} from 'react-router-dom'
+
+function NavbarItem( {text, link} ){
+    var nb_Item = (
+        <div className = 'NavbarItem'>
+            <NavLink exact to = { link }> { text } </NavLink>
+        </div>
+    )
+
+    return nb_Item
+}
 
 export const Navbar = () => {
     return (
-        <div className = 'Navbar'> 
-            
+        <div className = 'Navbar'>
+            <NavbarItem text = 'HOME' link = '/'/>
+            <div style ={{ backgroundColor: 'blue', height: 60, width: '1px', border: 'solid 2px blue' ,borderRadius: 10, margin: '0 15px'}} ></div>
+            <NavbarItem text = 'SORTING ALGRITHMS' link = '/sorting-algorithms'/>
+            <NavbarItem text = 'MATRIX TO TREE' link = '/matrix-to-tree'/>
+            <NavbarItem text = 'GRAPH TO MATRIX' link = '/graph-to-matrix'/>
         </div>
     )
+}
+
+NavbarItem.defaulProps = {
+    text: "link",
+    link: '/',
 }
